c8cacebc1d9a3816e7c4637fd8ba0dec828eb2af
YasiruR <wyasirurathsara@gmail.com>
2024-04-25 09:16:12 +0200

    Pipeline (#4)
    * :zap: added checkout
    
    * :zap: used lint repo action
    
    * :zap: added setup go action
    
    * :zap: mv workflow to cc
    
    * :zap: separate cc lint action
    
    * :zap: added checkout action
    
    * :zap: lint reusable workflow
    
    * :zap: added workflow_call
    
    * :zap: added sparse_checkout
    
    * :zap: added mv cc
    
    * :zap: lowercase repo name
    
    * :zap: github ref for lc var
    
    * :bug: test logs
    
    * :bug: test logs
    
    * :bug: test logs
    
    * :zap: sparse checkout for image push
    
    * :zap: path for img push
    
    * :zap: abs path in docker copy
    
    * :zap: added context img push
    
    * :zap: modified context path img push
    
    * :zap: added checkout for img push
    
    * :zap: changed workflow name
    
    * :sparkles: added manifest to workflow
    
    * :zap: test pwd
    
    * :zap: write path manifest
    
    * :zap: manifest script updated with root dir
    
    * :zap: paths updated in workflow
    
    * :zap: added lint build jobs
    
    * :zap: added lint build jobs
    
    * :zap: minor rm
    
    * :zap: added info log ccaas
    
    * :zap: fixed info log ccaas

--- chaincode/start.go
+++ chaincode/start.go
@@ -11,6 +11,7 @@ import (
 
 func main() {
 	/* invoke chaincode as an external service */
+	log.Info(`starting chaincode as an external service`)
 	assetCC, err := contractapi.NewChaincode(&asset.SmartContract{})
 	if err != nil {
 		log.Fatal(fmt.Sprintf(`creating chaincode failed - %v`, err))
